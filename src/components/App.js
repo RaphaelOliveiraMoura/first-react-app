3acb8b0fd06b6efa0fa8ccb8024a0801eb0b5ee7
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-07-17 13:59:07 -0300

    styled components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-
+import styled from 'styled-components';
 import Router from '../routes';
 
+const Rooter = styled.div`
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif
+`;
+
+const Header = styled.div`
+  width: 100vw;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Footer = styled.div`
+
+`;
+
 export default () => (
-  <div>
-    <h1>React App Header</h1>
+  <Rooter>
+    <Header>
+      <h1>React App Header</h1>
+    </Header>
     <Router />
-    <h3>Footer</h3>
-  </div>
+    <Footer />
+  </Rooter>
 );
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,39 @@
 import React from "react";
-
+import styled from 'styled-components';
 import Comment from './Comment';
+
+const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  form {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  input {
+    width: 100%;
+    border-radius: 6px;
+    outline: none;
+    padding: 4px 8px;
+    border-style: solid;
+    border-width: 1px;
+    border-color: #0a0a0a;
+  }
+
+  button {
+    border: none;
+    color: #FFF;
+    background-color: #9594bbcf;
+    padding: 6px 12px;
+    border-radius: 4px;
+    margin: 4px;
+    cursor: pointer;
+  }
+`;
 
 export default class Post extends React.Component {
 
@@ -36,18 +69,18 @@ export default class Post extends React.Component {
 
   render() {
     return (
-      <div>
+      <PostContainer>
         <h2>{this.props.title}</h2>
         <form onSubmit={this.onSubmitHandle}>
           <input
             value={this.state.currentInputText}
-            onChange={this.onInputChange}></input>
+            onChange={this.onInputChange} required></input>
           <button type="submit">Comentar</button>
         </form>
         {this.state.comments.map((comment, index) =>
           <Comment key={index} text={comment} />
         )}
-      </div>
+      </PostContainer>
     );
   }
 }
